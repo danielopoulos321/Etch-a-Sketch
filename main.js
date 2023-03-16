a53c5ba616967a5cf603e37099e7ae94aef84bbd
Daniel Anestopoulos <dstops97@gmail.com>
2023-03-16 11:41:09 -0400

    Add menu+align, Add slider

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('#gridbox');
 
 function populateGrid(boxNum) {
-
     for(let i = 0; i < boxNum; i++){
         const box = document.createElement('div');
         box.classList.add('box');
@@ -13,13 +12,16 @@ function populateGrid(boxNum) {
 }
 
 function adjustGridSize(){
-    let size = prompt("New Grid Size?");
+    let size = 0;
+    do {
+        size = prompt("New Grid Size? (1 - 100)");
+    } while (size < 1 || size > 100);
+
     container.innerHTML = '';
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     populateGrid(size*size)
 }
-
 
 
 populateGrid(256);
