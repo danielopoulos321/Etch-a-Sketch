afb711d035a4ed1f15ff34c12dfe513337ebd149
Daniel Anestopoulos <dstops97@gmail.com>
2023-03-16 13:39:37 -0400

    Finish opacity Toggle

--- main.js
+++ main.js
@@ -1,35 +1,53 @@
 const container = document.querySelector('#gridbox');
 
+const opacityButton = document.getElementById('opacity');
+let isOn = false;
+opacityButton.addEventListener('click', () => {
+    if(isOn) {
+        isOn = false;
+        opacityButton.textContent = 'Off';
+    } else {
+        isOn = true;
+        opacityButton.textContent = 'On';
+    }
+})
+
+
 function populateGrid(boxNum) {
     for(let i = 0; i < boxNum; i++){
         const box = document.createElement('div');
         box.classList.add('box');
         box.addEventListener("mouseover", () => {
-            box.style.backgroundColor = "black";
+            const color = document.getElementById('colorpicker').value;
+            if (isOn){
+                const opacity = Number(box.style.opacity);
+                if (opacity < 1) {
+                    box.style.opacity = (opacity + 0.1).toString();
+                }
+            }
+            box.style.backgroundColor = color;
+
         });
         container.appendChild(box);
     }
 }
 
-function adjustGridSize(){
-    let size = 0;
-    do {
-        size = prompt("New Grid Size? (1 - 100)");
-    } while (size < 1 || size > 100);
-
+function adjustGridSize(size){
     container.innerHTML = '';
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     populateGrid(size*size)
 }
 
+
 populateGrid(256);
 
 var slider = document.getElementById("gridrange");
 var output = document.getElementById("gridsize");
-output.innerHTML = slider.value + " X " + slider.value; // Display the default slider value
+output.innerHTML = "Grid Size: " + slider.value + " X " + slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value + " X " + this.value;
+  output.innerHTML = "Grid Size: " + this.value + " X " + this.value;
+  adjustGridSize(this.value);
 }
